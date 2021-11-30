79e3d38fbe19f94ad43bb5de3335746530f2877c
Nahidul Islam <nahidkishore99.com>
2021-11-30 21:04:31 +0600

    practice css framework

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5c6ac4',
+        secondary: {
+          100: '#FEE2E2',
+          500: '#EF4444'
+        },
+        // ...
+      },
+      fontFamily: {
+        body:['Nunito']
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
